6939d5a68c3c161f366d2d7f64dd28c7dbe3aa83
Shriyashree Beura <167524633+shriyashree24@users.noreply.github.com>
2024-12-25 18:29:51 +0530

    Added newrelic.js

---
+++ frontend/newrelic.js
@@ -0,0 +1,10 @@
+exports.config = {
+  app_name: ['Frontend - MERN App'],
+  license_key: 'F4B8A736461A7C57DFF87062EA6247CC154FDC610056B779E2880454F7E221B1',
+  logging: {
+    level: 'info',
+  },
+  distributed_tracing: {
+    enabled: true,
+  },
+};
